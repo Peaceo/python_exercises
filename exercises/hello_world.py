6eeb893a709fbc45540d4264c23d82d9c8531214
peace <peaceoariyo@gmail.com>
2018-06-04 10:37:44 +0100

    Add existing file

---
+++ exercises/hello_world.py
@@ -0,0 +1,38 @@
+message= "Hello Python World!"
+print (message)
+
+message="what are you doing?"
+print(message)
+
+message = "Hello Python Crash Course reader!" 
+print(message)
+
+name = "ada lovelace" 
+print(name.title())
+
+first_name="peace"
+last_name="ariyo"
+full_name= last_name + " " + first_name
+
+print(full_name.title())
+
+print (" let\n\tlet")
+
+print("Albert Einstein once said, 'A person who never made a mistake never tried anything new.'")
+
+num=2+3
+bumb=4**3
+print(num,bumb)
+print (0.2+0.1)
+
+age = 23 
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+print (3/2)
+import this
+
+names=['tola','praise','precious','prevail','faith','peace']
+
+names.sort(reverse=True)
+print(names)
---
+++ exercises/learnlist.py
@@ -0,0 +1,9 @@
+courses=['HCI','JAVA','ARCHI','MICROS','DATA','PMGT','OPSYS']
+for course in courses:
+	print("You must be patient enought to learn " +course)
+	print("I can't wait to be a professional at " + course +".\n")
+print("Thanks to the programming experts")
+
+
+
+
